cae6e4b814042f9d77b7634c161ef7c06e72f280
karthikeyan manimaran <karthikeyan14june@gmail.com>
2024-03-03 13:44:23 +0530

    omg, added so many new stuff

--- scrap_chalo.py
+++ scrap_chalo.py
@@ -5,8 +5,10 @@ from selenium.webdriver.common.by import By
 import requests
 import os
 import polyline
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# Function to convert data to GeoJSON format
+# function to convert json used by chalo to standard geojson format
 def convert_to_geojson(data):
     features = []
     for stop in data["stopSequenceWithDetails"]:
@@ -27,13 +29,13 @@ def convert_to_geojson(data):
 
     encoded_polyline = data["polyline"]
 
-    # Decode the polyline
+    # decode the polyline
     decoded_polyline = polyline.decode(encoded_polyline)
 
-    # Swap latitude and longitude pairs
+    # swap latitude and longitude pairs
     swapped_decoded_polyline = [(lon, lat) for lat, lon in decoded_polyline]
 
-    # Convert swapped decoded polyline to GeoJSON format
+    # convert swapped decoded polyline to GeoJSON format
     geojson = {
         "type": "Feature",
         "properties": {
@@ -54,82 +56,125 @@ def convert_to_geojson(data):
 
     return feature_collection
 
+# route_numbers.txt should have all the route number - each route should be in separate line
+# place the route_numbers.txt file in the same folder as this python script
+
+# ------------------ Begin Script ------------------
+
+# open the route_numbers.txt file and read it's content
 with open("route_numbers.txt", "r") as file:
     route_numbers = file.readlines()
 
+# iterate through the routes
 for route_number in route_numbers:
 
+    print(f"Start: Route {route_number}", end="")
+
+    # start the chrome driver
     driver = webdriver.Chrome()
 
-# Navigate to Chalo URL
+    # opens chalo website - please note the URL - this script is designed to navigate from "https://chalo.com/app/live-tracking" - might change in future
     driver.get("https://chalo.com/app/live-tracking")
 
-    time.sleep(3)
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//div[contains(text(), 'Chennai')]")))
 
-    # Select Chennai is the focus city
+    # selecting Chennai as the focus city (make sure to select your focus city)
     select_chn = driver.find_element(By.XPATH, "//div[contains(text(), 'Chennai')]")
     select_chn.click()
 
-    time.sleep(3)
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//button[contains(text(), 'Get Started')]")))
 
-    # Click the Get Started button
+    # Click the Get Started button (just some navigation stuff) - again will have to be modified if chalo decides to change it's UI or navigation schema
     select_get_started = driver.find_element(By.XPATH, "//button[contains(text(), 'Get Started')]")
     select_get_started.click()
 
-    time.sleep(5)
-    # Search using the Route Number
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "MuiList-root")))
+
+    # this section finds the search bar and types the route number
     search_b = driver.find_element(By.CLASS_NAME, "searchInputs")
     select_searchbar = search_b.find_element(By.TAG_NAME, "input")
     select_searchbar.clear()  # Clear search bar
     select_searchbar.send_keys(route_number.strip())  # Strip to remove leading/trailing whitespaces
     
-    time.sleep(3)
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "searchInputs")))
 
-    # Click the first result
+    # this section looks at the first result of the search
+    # it then gets the route name that's in the first result
     first_result = driver.find_element(By.CLASS_NAME, "MuiList-root")
-    first_result.click()
+    check_route = first_result.find_element(By.CLASS_NAME, "route-name")
+    route_in_chalo = check_route.text
 
-    time.sleep(3)
+    # this section checks if the route number from our list matches with the first result
+    # if it matches, then the script executes to the next step
+    # if it doesn't match, then it logs the failed route into "failed_routes.txt" file and exits
 
-    network_requests = driver.execute_script("return window.performance.getEntries()")
+    if route_in_chalo.strip() == route_number.strip() or route_in_chalo.strip() == route_number.strip() + "  Deluxe":
 
-    time.sleep(3)
+        first_result.click()
 
-    # Find the URL of the file that starts with "routelive"
-    route_live_url = None
-    for request in network_requests:
-        if request["name"].startswith("https://chalo.com/app/api/scheduler_v4/v4/chennai/routedetailslive?route_id="):
-            route_live_url = request["name"]
-            break
+        time.sleep(5)
 
-    if route_live_url:
-        print("URL of routelive file:", route_live_url)
+        network_requests = driver.execute_script("return window.performance.getEntries()")
 
-        # Now you can download the file using the obtained URL
-        # For example, using requests library:
-        import requests
+        time.sleep(3)
 
-        response = requests.get(route_live_url)
-        if response.status_code == 200:
-            with open(f"routelive_{route_number.strip()}.json", "wb") as file:
-                file.write(response.content)
-            print("routelive file downloaded successfully.")
+        # Find the request URL that matches "https://chalo.com/app/api/scheduler_v4/v4/chennai/routedetailslive?route_id=" "
+        route_live_url = None
 
-            # Read input data from the downloaded routelive JSON file
-            with open(f"routelive_{route_number.strip()}.json", "r") as file:
-                json_data = json.load(file)['route']
+        for request in network_requests:
+            if request["name"].startswith("https://chalo.com/app/api/scheduler_v4/v4/chennai/routedetailslive?route_id="):
+                route_live_url = request["name"]
+                break
 
-            # Convert to GeoJSON format
-            geojson_data = convert_to_geojson(json_data)
+        if route_live_url:
 
-            # Output the GeoJSON data to a file
-            with open(f"route_{route_number.strip()}.geojson", "w") as output_file:
-                json.dump(geojson_data, output_file)
-            print("GeoJSON file created successfully.")
+            import requests
 
-            os.remove(f"routelive_{route_number.strip()}.json")
+            response = requests.get(route_live_url)
 
+            if response.status_code == 200:
+                with open(f"routelive_{route_number.strip()}.json", "wb") as file:
+                    file.write(response.content)
+
+                # Read input data from the downloaded routelive JSON file
+                with open(f"routelive_{route_number.strip()}.json", "r") as file:
+                    json_data = json.load(file)['route']
+
+                # Convert to GeoJSON format
+                geojson_data = convert_to_geojson(json_data)
+
+                # Output the GeoJSON data to a file
+                with open(f"route_{route_number.strip()}.geojson", "w") as output_file:
+                    json.dump(geojson_data, output_file)
+                print("GeoJSON file created successfully.")
+
+                os.remove(f"routelive_{route_number.strip()}.json")
+
+                print (f"Success! Route: {route_number}", end="")
+
+            else:
+                print("Failed to download routelive file. Status code:", response.status_code)
         else:
-            print("Failed to download routelive file. Status code:", response.status_code)
+            print("routelive file not found in network requests.")
     else:
-        print("routelive file not found in network requests.")
+
+        # For failed routes please check "failed_routes.txt" file
+        print (f"Failure! Route: {route_number}", end="")
+
+        file_path = "failed_routes.txt"
+
+        # Check if the file exists
+        if os.path.exists(file_path):
+            # Open the file in append mode and add content
+            with open(file_path, "a") as file:
+                file.write(route_number.strip() + "\n")
+        else:
+            # Open the file in write mode and create it
+            with open(file_path, "w") as file:
+                file.write(route_number.strip() + "\n")
+
+    print(f"End: Route {route_number}", end="")
+
+
+
+    
